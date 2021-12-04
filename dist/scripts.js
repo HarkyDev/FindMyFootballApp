8bd80acfa2a8bae7eebf3296e51e205947f0987e
Vienna Borowska <vienna.borowska@hotmail.com>
2021-12-04 10:09:26 +0000

    added marker to map

--- dist/scripts.js
+++ dist/scripts.js
@@ -101,28 +101,40 @@ var getAddress = function (teamId) {
       var teamLat = response.items[0].position.lat;
       var teamLong = response.items[0].position.lng;
 
-
-
       var platform = new H.service.Platform({
-        'apikey': myMapAPI
+        apikey: myMapAPI,
       });
-     // Obtain the default map types from the platform object:
+      // Obtain the default map types from the platform object:
       var defaultLayers = platform.createDefaultLayers();
 
-// Instantiate (and display) a map object:
-    var map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.vector.normal.map,
-    {
-      zoom: 10,
-      center: { lat: teamLat, lng: teamLong }
-    });
-    
-    });
+      // Instantiate (and display) a map object:
+      var map = new H.Map(
+        document.getElementById("mapContainer"),
+        defaultLayers.vector.normal.map,
+        {
+          zoom: 13,
+          center: { lat: teamLat, lng: teamLong },
+        }
+      );
 
-    
+      // SVG markup that define icon image
+      var svgMarkup =
+        '<svg width="24" height="24" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+        'height="22" /><text x="12" y="18" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white">!</text></svg>';
 
+      // Creating icon, object for coordinates and marker
+      var icon = new H.map.Icon(svgMarkup);
+      var coords = { lat: teamLat, lng: teamLong };
+      var marker = new H.map.Marker(coords, {icon: icon});
+
+      //add marker to map
+      map.addObject(marker);
     });
+  });
 };
 
 var premierLeagueFetch = function (userInput) {
